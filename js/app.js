9607b638a5cb09cecea88b33c44db1addff2dbf2
Vladislav <99101070+Vla9ik@users.noreply.github.com>
2023-12-29 17:53:43 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -454,7 +454,6 @@
     }
     (() => {
         "use strict";
-        const modules_flsModules = {};
         function isWebp() {
             function testWebP(callback) {
                 let webP = new Image;
@@ -521,113 +520,6 @@
                 }
             }));
         }
-        function functions_FLS(message) {
-            setTimeout((() => {
-                if (window.FLS) console.log(message);
-            }), 0);
-        }
-        function getDigFormat(item, sepp = " ") {
-            return item.toString().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, `$1${sepp}`);
-        }
-        function uniqArray(array) {
-            return array.filter((function(item, index, self) {
-                return self.indexOf(item) === index;
-            }));
-        }
-        class ScrollWatcher {
-            constructor(props) {
-                let defaultConfig = {
-                    logging: true
-                };
-                this.config = Object.assign(defaultConfig, props);
-                this.observer;
-                !document.documentElement.classList.contains("watcher") ? this.scrollWatcherRun() : null;
-            }
-            scrollWatcherUpdate() {
-                this.scrollWatcherRun();
-            }
-            scrollWatcherRun() {
-                document.documentElement.classList.add("watcher");
-                this.scrollWatcherConstructor(document.querySelectorAll("[data-watch]"));
-            }
-            scrollWatcherConstructor(items) {
-                if (items.length) {
-                    this.scrollWatcherLogging(`Прокинувся, стежу за об'єктами (${items.length})...`);
-                    let uniqParams = uniqArray(Array.from(items).map((function(item) {
-                        return `${item.dataset.watchRoot ? item.dataset.watchRoot : null}|${item.dataset.watchMargin ? item.dataset.watchMargin : "0px"}|${item.dataset.watchThreshold ? item.dataset.watchThreshold : 0}`;
-                    })));
-                    uniqParams.forEach((uniqParam => {
-                        let uniqParamArray = uniqParam.split("|");
-                        let paramsWatch = {
-                            root: uniqParamArray[0],
-                            margin: uniqParamArray[1],
-                            threshold: uniqParamArray[2]
-                        };
-                        let groupItems = Array.from(items).filter((function(item) {
-                            let watchRoot = item.dataset.watchRoot ? item.dataset.watchRoot : null;
-                            let watchMargin = item.dataset.watchMargin ? item.dataset.watchMargin : "0px";
-                            let watchThreshold = item.dataset.watchThreshold ? item.dataset.watchThreshold : 0;
-                            if (String(watchRoot) === paramsWatch.root && String(watchMargin) === paramsWatch.margin && String(watchThreshold) === paramsWatch.threshold) return item;
-                        }));
-                        let configWatcher = this.getScrollWatcherConfig(paramsWatch);
-                        this.scrollWatcherInit(groupItems, configWatcher);
-                    }));
-                } else this.scrollWatcherLogging("Сплю, немає об'єктів для стеження. ZzzZZzz");
-            }
-            getScrollWatcherConfig(paramsWatch) {
-                let configWatcher = {};
-                if (document.querySelector(paramsWatch.root)) configWatcher.root = document.querySelector(paramsWatch.root); else if (paramsWatch.root !== "null") this.scrollWatcherLogging(`Эмм... батьківського об'єкта ${paramsWatch.root} немає на сторінці`);
-                configWatcher.rootMargin = paramsWatch.margin;
-                if (paramsWatch.margin.indexOf("px") < 0 && paramsWatch.margin.indexOf("%") < 0) {
-                    this.scrollWatcherLogging(`йой, налаштування data-watch-margin потрібно задавати в PX або %`);
-                    return;
-                }
-                if (paramsWatch.threshold === "prx") {
-                    paramsWatch.threshold = [];
-                    for (let i = 0; i <= 1; i += .005) paramsWatch.threshold.push(i);
-                } else paramsWatch.threshold = paramsWatch.threshold.split(",");
-                configWatcher.threshold = paramsWatch.threshold;
-                return configWatcher;
-            }
-            scrollWatcherCreate(configWatcher) {
-                this.observer = new IntersectionObserver(((entries, observer) => {
-                    entries.forEach((entry => {
-                        this.scrollWatcherCallback(entry, observer);
-                    }));
-                }), configWatcher);
-            }
-            scrollWatcherInit(items, configWatcher) {
-                this.scrollWatcherCreate(configWatcher);
-                items.forEach((item => this.observer.observe(item)));
-            }
-            scrollWatcherIntersecting(entry, targetElement) {
-                if (entry.isIntersecting) {
-                    !targetElement.classList.contains("_watcher-view") ? targetElement.classList.add("_watcher-view") : null;
-                    this.scrollWatcherLogging(`Я бачу ${targetElement.classList}, додав клас _watcher-view`);
-                } else {
-                    targetElement.classList.contains("_watcher-view") ? targetElement.classList.remove("_watcher-view") : null;
-                    this.scrollWatcherLogging(`Я не бачу ${targetElement.classList}, прибрав клас _watcher-view`);
-                }
-            }
-            scrollWatcherOff(targetElement, observer) {
-                observer.unobserve(targetElement);
-                this.scrollWatcherLogging(`Я перестав стежити за ${targetElement.classList}`);
-            }
-            scrollWatcherLogging(message) {
-                this.config.logging ? functions_FLS(`[Спостерігач]: ${message}`) : null;
-            }
-            scrollWatcherCallback(entry, observer) {
-                const targetElement = entry.target;
-                this.scrollWatcherIntersecting(entry, targetElement);
-                targetElement.hasAttribute("data-watch-once") && entry.isIntersecting ? this.scrollWatcherOff(targetElement, observer) : null;
-                document.dispatchEvent(new CustomEvent("watcherCallback", {
-                    detail: {
-                        entry
-                    }
-                }));
-            }
-        }
-        modules_flsModules.watcher = new ScrollWatcher({});
         let addWindowScrollEvent = false;
         function headerScroll() {
             addWindowScrollEvent = true;
@@ -655,37 +547,6 @@
                 scrollDirection = scrollTop <= 0 ? 0 : scrollTop;
             }));
         }
-        function digitsCounter() {
-            function digitsCountersInit(digitsCountersItems) {
-                let digitsCounters = digitsCountersItems ? digitsCountersItems : document.querySelectorAll("[data-digits-counter]");
-                if (digitsCounters.length) digitsCounters.forEach((digitsCounter => {
-                    digitsCounter.dataset.digitsCounter = digitsCounter.innerHTML;
-                    digitsCounter.innerHTML = `0`;
-                    digitsCountersAnimate(digitsCounter);
-                }));
-            }
-            function digitsCountersAnimate(digitsCounter) {
-                let startTimestamp = null;
-                const duration = parseFloat(digitsCounter.dataset.digitsCounterSpeed) ? parseFloat(digitsCounter.dataset.digitsCounterSpeed) : 1e3;
-                const startValue = parseFloat(digitsCounter.dataset.digitsCounter);
-                const format = digitsCounter.dataset.digitsCounterFormat ? digitsCounter.dataset.digitsCounterFormat : " ";
-                const startPosition = 0;
-                const step = timestamp => {
-                    if (!startTimestamp) startTimestamp = timestamp;
-                    const progress = Math.min((timestamp - startTimestamp) / duration, 1);
-                    const value = Math.floor(progress * (startPosition + startValue));
-                    digitsCounter.innerHTML = typeof digitsCounter.dataset.digitsCounterFormat !== "undefined" ? getDigFormat(value, format) : value;
-                    if (progress < 1) window.requestAnimationFrame(step);
-                };
-                window.requestAnimationFrame(step);
-            }
-            function digitsCounterAction(e) {
-                const entry = e.detail.entry;
-                const targetElement = entry.target;
-                if (targetElement.querySelectorAll("[data-digits-counter]").length) digitsCountersInit(targetElement.querySelectorAll("[data-digits-counter]"));
-            }
-            document.addEventListener("watcherCallback", digitsCounterAction);
-        }
         setTimeout((() => {
             if (addWindowScrollEvent) {
                 let windowScroll = new Event("windowScroll");
@@ -696,11 +557,59 @@
         }), 0);
         var aos = __webpack_require__(711);
         aos.init();
+        function isElementVisible(el) {
+            const rect = el.getBoundingClientRect();
+            return rect.top >= 0 && rect.left >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && rect.right <= (window.innerWidth || document.documentElement.clientWidth);
+        }
+        function numCounter(selector, number, time, step) {
+            const counter = document.querySelector(selector);
+            let res = 0;
+            const allTime = Math.round(time / (number / step));
+            let interval = setInterval((() => {
+                res += step;
+                if (res >= number) {
+                    res = number;
+                    clearInterval(interval);
+                }
+                counter.innerHTML = res;
+            }), allTime);
+        }
+        const countersInfo = {
+            "#num1": {
+                number: 70,
+                time: 1500,
+                step: 1,
+                initialized: false
+            },
+            "#num2": {
+                number: 250,
+                time: 1500,
+                step: 1,
+                initialized: false
+            },
+            "#num3": {
+                number: 70,
+                time: 1500,
+                step: 1,
+                initialized: false
+            }
+        };
+        window.addEventListener("scroll", (() => {
+            for (const selector in countersInfo) {
+                const info = countersInfo[selector];
+                if (!info.initialized) {
+                    const element = document.querySelector(selector);
+                    if (isElementVisible(element)) {
+                        numCounter(selector, info.number, info.time, info.step);
+                        info.initialized = true;
+                    }
+                }
+            }
+        }));
         window["FLS"] = true;
         isWebp();
         addLoadedClass();
         menuInit();
         headerScroll();
-        digitsCounter();
     })();
 })();
